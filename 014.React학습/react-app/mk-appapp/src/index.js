8b92521cc0399bdecb1c14d4e560378725a2c324
kmmkyung <01aldrn10@gmail.com>
2023-06-01 13:20:24 +0900

    s

--- 014.React학습/react-app/mk-appapp/src/index.js
+++ 014.React학습/react-app/mk-appapp/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Test from './Test';
+import Ban from './DC/Ban';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root'));
-root.render(<Test/>)
+root.render(<Ban/>)
