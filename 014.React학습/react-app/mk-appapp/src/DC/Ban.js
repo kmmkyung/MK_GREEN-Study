f3c321578edd076b9fe876f0aa1951ac5fae275d
kmmkyung <01aldrn10@gmail.com>
2023-06-02 12:08:52 +0900

    s

--- 014.React학습/react-app/mk-appapp/src/DC/Ban.js
+++ 014.React학습/react-app/mk-appapp/src/DC/Ban.js
@@ -3,11 +3,66 @@
 import "./css/ban.css"
 // 배너 데이터
 import ban_data from "./data/banner";
+// 제이쿼리
+import $ from "jquery"
+
+$(()=>{ ////// JQB //////
+
+    // 광클 금지 변수
+    let prot = 0;
+
+    // 1. 버튼 클릭시 이동기능 구현
+    $(".abtn").click(function(){
+
+        // 광클 금지
+        if(prot) return;
+        prot=1;
+        // 0.4초 있다가 다시 prot 0으로!
+        setTimeout(()=>prot=0,400);
+
+        // 1. 버튼 구분하기
+        let isB = $(this).is(".rb")
+        console.log('오른쪽버튼?',isB);  
+
+        // 1-2. 슬라이드 타겟 설정 : 클릭된 버튼의 형제 요소 슬라이더
+        const tg = $(this).siblings(".slider")
+
+        // 2. 분기하여 기능구현하기
+        // (1) 오른쪽 버튼 클릭시: 오른쪽에서 0 -> left:-100으로 들어옴
+        if(isB){
+                //오른쪽 버튼을 누르면 .4초동안 -100% 하고 아래 함수 호출! 
+            tg.animate({left:"-100%"},400,function(){ // this는 tg!
+                // 첫번째 li 맨 뒤로 보내기
+                $(this).append($(this).find("li").first())
+                // css 튀지않게 맞춰줌!
+                .css({left:"0%"});
+            }); ///// animate /////
+        } ///// if /////
+        
+        // (2) 왼쪽 버튼 클릭시: 왼쪽에서 들어옴(left:-100%->0)
+        else{
+            // 마지막 li 맨앞이동 + 동시에 left -100% 후 0%로 애니
+            tg.prepend(tg.find("li").last())
+            .css({left:"-100%"})
+            .animate({left:"0%"},400)
+        } ///// else /////
+
+        // 3. 배너와 일치하는 블릿에 클래스 "on" 넣기!
+        // 대상: .indic li(블릿)
+        // eq(순번) -> 오른쪽 이동시 1, 왼쪽 이동시 0
+        // isB값으로 오른쪽은 true -> 1, 왼쪽은 false->0
+        // 순서가 바뀌는 슬라이드에 고유 순번속성 data-seq값을 읽어옴
+        $(".indic li").eq(tg.find("li").eq(isB).attr("data-seq")).addClass("on").siblings().removeClass("on");
+        
+    }); ///// --- click --- /////
+
+
+}); ////// JQB //////
 
 // 반복리스트 코드 생성용 컴포넌트 //////
 function MakeList(props){ // rec - 개별레코드값(객체형식)
     return(
-        <li>
+        <li data-seq={props.idx}>
             <img className="banimg" src={props.rec["src"]} alt="배너"/>
             <section className="bantit">
                 <h3>{props.rec["tit1"]}</h3>
@@ -27,9 +82,31 @@ function Ban(props){ // props.cat은 배너 데이터 구분 속성명
 
     return(
         <div className="banner">
+            {/* 이동 슬라이드 */}
             <ul className="slider">
-                {sel_data.map((x,i)=> <MakeList rec={x} key={i}/>)}
+                {sel_data.map((x,i)=> <MakeList rec={x} key={i} idx={i}/>)}
             </ul>
+            {/* 이동 버튼 + 슬라이드 블릿 : 슬라이드가 2개 이상 */}
+            {/* 조건식 && 코드: 조건식이 true일떄 코드출력 */}
+            {
+                sel_data.length > 1 && (
+                // 하나에 싸자!
+                <>
+                    {/* 양쪽이동버튼 */}
+                    <button className="abtn lb">＜</button>
+                    <button className="abtn rb">＞</button> 
+                    {/* 블릿 인디케이터(데이터 개수만큼!) */}
+                    <ol className="indic">
+                        {
+                            sel_data.map((x,i)=>(
+                                // i 첫번째꺼면 클래스 on을 넣고 아니면 넣지마!
+                                <li className={i==0?'on':''} key={i}></li>
+                            ))
+                        }
+                    </ol>
+                </>
+                )
+            }
         </div>
     );
 } ////////// Ban 컴포넌트 ///////////
