47ba80ba1eef8fb4b934f69d36f135d6c51ae868
kmmkyung <01aldrn10@gmail.com>
2023-06-08 11:59:08 +0900

    s

---
+++ 700. 쉐도잉 프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/data/swipervid.js
@@ -0,0 +1,31 @@
+// Swipervid
+
+const swipervid_data = [
+    {
+        iscr:"",
+        vsrc:"",
+        cat:"",
+        tit:"gkgk",
+    },
+    {
+        iscr:"",
+        vsrc:"",
+        cat:"",
+        tit:"gkgk",
+    },
+    {
+        iscr:"",
+        vsrc:"",
+        cat:"",
+        tit:"gkgk",
+    },
+    {
+        iscr:"",
+        vsrc:"",
+        cat:"",
+        tit:"gkgk",
+    },
+]
+
+// 데이터 내보내기
+export default swipervid_data;
---
+++ 700. 쉐도잉 프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/VidSwiper.js
@@ -0,0 +1,38 @@
+///  VidSwipe 모듈 - VidSwipe.js
+import $ from 'jquery';
+import "../css/vidswiper.css";
+import SwiperVid from '../plugin/SwiperVid';
+
+// 제이쿼리 로드구역 함수 /////////
+function jqFn(){
+    $(()=>{
+
+    }); //////// jQB ///////////
+} ////////////// jQFn ///////////
+
+function VidSwiper(props){
+    // props.pg - 페이지별 데이터구분
+    // props.tit - 모듈타이틀
+    return(
+        <>
+        <section className='vidswdox'>
+            {/* 1. 모듈타이틀 */}
+            <h2 className='tit'>{props.tit}</h2>
+
+            {/* 2. 스와이퍼 컴포넌트 */}
+            <SwiperVid name="나는" />
+
+            {/* 3. 비디오 재생창 */}
+            <section className='playvid'>
+                <iframe src=''></iframe>
+                <button className='cbtn'>×</button>
+            </section>
+        </section>
+
+        {/* 빈루트를 만들고 JS로드함수포함 */}
+        {jqFn()}
+        </>
+    )
+}
+
+export default VidSwiper;
