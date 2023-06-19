121959a02a08d6d1fb2603f079f3fa9ec7097d26
kmmkyung <01aldrn10@gmail.com>
2023-06-19 12:37:26 +0900

    s

--- 700. 쉐도잉 프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Detail.js
+++ 700. 쉐도잉 프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Detail.js
@@ -1,10 +1,33 @@
 // 상세페이지 컴포넌트 - Detail.js
 // 라우터 파라미터값 받아서 데이터 처리!
 
+import { useLocation } from "react-router-dom";
+
 function Detail(props){
+  // 라우터 전달값을 받기위한 useLocation 생성하기
+  const loc = useLocation();
+  // 보낸 속성명을 변수에 할당하기
+  // state.속성명 : 내가 라우터를 통해 보낸 속성값 받기
+  // 1. 캐릭터 이름 / 2. 캐릭터 설명-^문자로 잘라 배열로 변경 / 3. 캐릭터 명세
+  const cname = loc.state.cname;
+  
+  let cdesc = loc.state.cdese;
+  cdesc = cdesc.split('^');
+
+  const facts = loc.state.facts;
+  
   return(
     <>
-      <h2>나는 상세페이지야</h2>
+      <h2>{cname}</h2>
+      <div className="cdesc">
+        {
+        cdesc.map(v=><p>{v}</p>)
+        }
+        </div>
+      <div className="facts">
+        <h3>CHARACTER FACTS</h3>
+        {facts}  
+      </div>
     </>
   )
 }; ////////// Detail ////////////
--- 700. 쉐도잉 프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/plugin/SwiperCat.js
+++ 700. 쉐도잉 프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/plugin/SwiperCat.js
@@ -7,14 +7,13 @@ import $ from "jquery";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-
-
 import "./swipercat.css";
 
 // import required modules
 import { Navigation } from "swiper";
 // 데이터 가져오기
 import cat_data from "../data/cat";
+import { Link } from "react-router-dom";
 
 export default function SwiperCat(props) {
     // 데이터 셋팅
@@ -45,6 +44,19 @@ export default function SwiperCat(props) {
                 className="mySwiper">
                 {sdt.map((v, i) => (
                     <SwiperSlide key={i}>
+                        {/* "/det" 라우터 컴포넌트 페이지 호출시 state 속성값으로 객체를 보내어 값을 전달함!
+                        도착페이지인 Detail.js 컴포넌트에 페이지 나타내야할 데이터 항목을
+                        데이터 속성명과 같은 이름으로 셋팅하여 라우터 전달 state 객체에 담아서 보낸다!
+                        cname은 캐릭터 이름 /  cdese는 캐락터 설명 / facts는 캐릭터 명세 정보임!
+                        selcat은 캐릭터 이름전달 속성값
+                        값은 v.cname으로 캐릭터 명을 보낸다 */}
+                        <Link to="/det" state={
+                            {
+                            cname:v.cname,
+                            cdesc:v.cdesc,
+                            fachs:v.facts}
+                            }>
+
                         <section className="swinbx">
                             {/* 캐릭터이미지영역 */}
                             <div className="catimg">
@@ -56,6 +68,7 @@ export default function SwiperCat(props) {
                                 <h3>{v.cname}</h3>
                             </div>
                         </section>
+                        </Link>
                     </SwiperSlide>
                 ))}
             </Swiper>
